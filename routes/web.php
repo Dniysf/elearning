7ddf3f24d57eebbde96328737d3586e7280155a2
Dniysf <deniyusuf00@gmail.com>
2024-05-15 11:32:19 +0700

    Laravel

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+/** Jenis Method HTTP:
+ * 1. GET= menampilkan halaman
+ * 2. POST= mengirim data
+ * 3. PUT= meng-update data
+ * 4. DELETE= menghapus data
+ */
+//route baru untuk menampilkan teks 
+Route::get('/Hallo/{nama}', function($nama){
+    return "Assalamualaikum...$nama";
+});
+
+
+Route::get('admin/Dashboard', [DashboardController::class, 'index']);
+    
+//route::get("Profile", function(){
+    //return view("Profile");
+//});
